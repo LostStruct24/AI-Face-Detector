d67fba3e94cadd30d38a11bc49e32528a13113e6
N <75640078+N12345678987654321@users.noreply.github.com>
2021-06-13 23:42:43 +0400

    face detector

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-//import Rank from './components/Rank/Rank';
 import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
